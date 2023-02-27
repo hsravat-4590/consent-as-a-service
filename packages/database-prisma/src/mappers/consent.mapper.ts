b2117a30aa9761f3638c686829f58090340a0c48
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2023-02-27 14:22:50 +0000

    Implement an Open Consent DA & Update Schemas
    Signed-off-by: Hanzalah Ravat <hanzalah.ravat@gmail.com>

---
+++ packages/database-prisma/src/mappers/consent.mapper.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2023 Hanzalah Ravat
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { Consent, TxnLog } from "@prisma/client";
+import { mapTxnLogToModel } from "./txn-log.mapper";
+import { ConsentModel } from "@consent-as-a-service/domain";
+import { convertDateToLocalDateTime } from "./util-type.mapper";
+
+export const mapConsentToModel = (consent: Consent, txnLog: TxnLog) => {
+  const txnModel = mapTxnLogToModel(txnLog);
+  return {
+    id: consent.consentId,
+    consentRequestId: consent.consentRequestId,
+    //TODO UserDeepMapping
+    //TODO OrgDeepMapping
+    expiry: convertDateToLocalDateTime(consent.expiry),
+    transaction: txnModel,
+  } as ConsentModel;
+};

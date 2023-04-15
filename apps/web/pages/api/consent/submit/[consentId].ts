2b3b4c79767405ced8501234ebd26c229dda5997
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2023-04-15 18:35:08 +0100

    Submit Server Side Endpoint
    Signed-off-by: Hanzalah Ravat <hanzalah.ravat@gmail.com>

---
+++ apps/web/pages/api/consent/submit/[consentId].ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2023 Hanzalah Ravat
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import ServerConfig from "../../service/server.config";
+import completeCommon from "../complete-common";
+
+export default withApiAuthRequired(async function submitConsent(req, res) {
+  const { accessToken } = await getAccessToken(req, res);
+  const body = JSON.parse(req.body);
+  const { consentId } = req.query;
+  console.log(`Body is ${req.body}`);
+  const response = await fetch(
+    `${ServerConfig.baseUrl}:${ServerConfig.port}/consent/user/v1/submit/${consentId}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: req.body,
+    }
+  );
+  await completeCommon(res, response);
+  return Promise.resolve();
+});

1ce3cdba24eda4f7f47185d0279b709fc4af6926
Hanzalah Ravat <hanzalah.ravat@gmail.com>
2023-04-28 11:16:47 +0100

    Simple Demo Frontend Improvement
    Signed-off-by: Hanzalah Ravat <hanzalah.ravat@gmail.com>

--- apps/demo/src/App.svelte
+++ apps/demo/src/App.svelte
@@ -3,8 +3,9 @@
   import Card from "@smui/card";
 
   let token: string = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IlVmSTdUcmhpU1JZdDlfWVI4ZmRWOSJ9.eyJpc3MiOiJodHRwczovL2Rldi1vaXhrb28yNnk2Zm44c2JkLnVrLmF1dGgwLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDEwMDYzMzkwMzc4MTc3NzUxOTQ2NiIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjMwMDMiLCJodHRwczovL2Rldi1vaXhrb28yNnk2Zm44c2JkLnVrLmF1dGgwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2ODI2MjU1NzMsImV4cCI6MTY4MjcxMTk3MywiYXpwIjoiMWNvRFpXWFdYcFV1QjBRaU9GUm91azhMbE5hb3hUcXoiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG9mZmxpbmVfYWNjZXNzIn0.l85ldYdX-h1ph_Hy7Lctm32k4dhr6LtkoIKdx-BNICBchP8puo0FuSE9i0kDwAZw5g1OoKPas7GLqwNNsCxFKkUyBk__1XUg1CJ54VnzIOJNS_0et8uouUanY-8CvDrC77i652Pve7YWnm5STKKH8EtUvVRJ1xuT5pBzzvU5IdCKhbc-fNLrkDhkMt4lyRpAwunhw7gKw0AyogcicPN5w0MgbvU1XRfi0s6Lz7mZ53kqXTtetWEqYtZVlIqFyJaygqFqVdhzm0aSiIE1tkh4e0saukuZaxJSTHcVllT-01UWYPc-eSoXz96JPhRTINh7FjfPy32FAV3UDVsZhGA3FA"
-  let requestId: string = "d6b9a1c4-f82a-4b51-a708-0519513675b7"
-  let pendingConsentId: string = "";
+  let requestId: string = "744bfcd5-04ee-4a76-bbbf-82a4d91e834c"
+  let pendingConsentId: string = localStorage.getItem('consentId')
+  let url = localStorage.getItem('requestUrl')
   let pendingConsent: any;
   let consentData: any
   const createPendingConsent = async () =>  {
@@ -17,6 +18,8 @@
     const json = await response.json();
     pendingConsent = json
     pendingConsentId = pendingConsent.consentId
+    localStorage.setItem('consentId', pendingConsentId)
+    localStorage.setItem('requestUrl', pendingConsent.request_url)
     console.log(JSON.stringify(json))
   }
 
@@ -41,7 +44,7 @@
   }
 
   const getConsentData = async () => {
-    const response = await fetch(`http://localhost:3003/consent/data/v1/${requestId}`, {
+    const response = await fetch(`http://localhost:3003/consent/data/v1/${pendingConsentId}`, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${token}`,
@@ -50,6 +53,10 @@
     const json = await response.json();
     console.log(json)
     consentData = JSON.stringify(json)
+  }
+
+  const goToConsentRequest = () => {
+    window.location.href= pendingConsent.request_url
   }
 </script>
 
@@ -60,6 +67,7 @@
     <h2>Access Token</h2>
     <input bind:value={token}>
   </Card>
+
   <Card>
     <h2>Consent Request ID</h2>
     <input bind:value={requestId}>
@@ -69,7 +77,7 @@
 <!--    <h2>Access Token</h2>-->
 <!--    <input bind:value={token}>-->
 <!--  </div>-->
-
+  <h3> </h3>
   <Button on:click= {() => createPendingConsent()}>
     Create Pending Consent
   </Button>
@@ -81,6 +89,9 @@
 
   <Button on:click= {() => getStatus()}>
     Get Status
+  </Button>
+  <Button on:click= {() => goToConsentRequest()}>
+    Go to Consent Request
   </Button>
 
   <Button on:click= {() => getConsentData()}>
